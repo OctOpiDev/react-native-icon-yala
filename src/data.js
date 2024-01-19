0e2b552790abd461a6c16513bbb6a9f2aafe16b4
OctOpi <64381167+OctOpiDev@users.noreply.github.com>
2024-01-19 16:24:24 +0300

    flag update

--- src/data.js
+++ src/data.js
@@ -28,4 +28,128 @@ module.exports = {
 	<path d="M0 0H60V20H0V0Z" fill="#31358E"/>
 	</svg>	
 `,
+	'foxtrot': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H60V40H0V0Z" fill="#C4C4C4"/>
+	<path d="M0 0H60V40H0V0Z" fill="white"/>
+	<path d="M30 0L60 20L30 40L0 20L30 0Z" fill="#ED1B24"/>
+	</svg>	
+`,
+	'golf': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H9.78541V40H0V0Z" fill="#FCF400"/>
+	<path d="M10.0429 0H19.8283V40H10.0429V0Z" fill="#31358E"/>
+	<path d="M20.0859 0H29.8713V40H20.0859V0Z" fill="#FCF400"/>
+	<path d="M30.1287 0H39.9141V40H30.1287V0Z" fill="#31358E"/>
+	<path d="M40.1716 0H49.957V40H40.1716V0Z" fill="#FCF400"/>
+	<path d="M50.2146 0H60V40H50.2146V0Z" fill="#31358E"/>
+	</svg>	
+`,
+	'hotel': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H30V40H0V0Z" fill="white"/>
+	<path d="M30 0H60V40H30V0Z" fill="#ED1B24"/>
+	</svg>
+`,
+	'india': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H60V40H0V0Z" fill="#FCF400"/>
+	<ellipse cx="30.3947" cy="20.3704" rx="6.71053" ry="6.2963" fill="black"/>
+	</svg>
+`,
+	'juliett': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 13.3333H60V26.6667H0V13.3333Z" fill="white"/>
+	<path d="M0 0H60V13.3333H0V0Z" fill="#31358E"/>
+	<path d="M0 26.6667H60V40H0V26.6667Z" fill="#31358E"/>
+	</svg>
+`,
+	'kilo': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H30V40H0V0Z" fill="#FCF400"/>
+	<path d="M30 0H60V40H30V0Z" fill="#31358E"/>
+	</svg>
+`,
+	'lima': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H30V20H0V0Z" fill="#FCF400"/>
+	<path d="M30 0H60V20H30V0Z" fill="black"/>
+	<path d="M60 20H30V40H60V20Z" fill="#FCF400"/>
+	<path d="M30 20H-1.90735e-06V40H30V20Z" fill="black"/>
+	</svg>
+`,
+	'november': `<svg width="62" height="42" viewBox="0 0 62 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M1 0.5H0.5V1V41V41.5H1H61H61.5V41V1V0.5H61H1Z" fill="#C4C4C4" stroke="black"/>
+	</svg>
+`,
+	'yankee': `<svg width="62" height="42" viewBox="0 0 62 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M1 0.5H0.5V1V41V41.5H1H61H61.5V41V1V0.5H61H1Z" fill="#C4C4C4" stroke="black"/>
+	</svg>
+`,
+	'mike': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H60V40H0V0Z" fill="#31358E"/>
+	<path fill-rule="evenodd" clip-rule="evenodd" d="M7.3623 0H0V6.50378L52.858 40H60V33.3566L7.3623 0Z" fill="white"/>
+	<path fill-rule="evenodd" clip-rule="evenodd" d="M52.6377 0H60V6.50378L7.14198 40H-3.8147e-06V33.3566L52.6377 0Z" fill="white"/>
+	</svg>
+`,
+	'oscar': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M60 0H0L60 40V0Z" fill="#ED1B24"/>
+	<path d="M-3.8147e-06 40H60L-3.8147e-06 0V40Z" fill="#FCF400"/>
+	</svg>
+`,
+	'papa': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H60V40H0V0Z" fill="#31358E"/>
+	<path d="M19.7368 13.3333H39.7368V26.6667H19.7368V13.3333Z" fill="white"/>
+	</svg>
+`,
+	'quebec': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H60V40H0V0Z" fill="#FCF400"/>
+	</svg>
+	`,
+	'romeo': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H60V40H0V0Z" fill="#FCF400"/>
+	<path d="M0 25.1852H24.4737V40H0V25.1852Z" fill="#ED1B24"/>
+	<path d="M0 0H24.4737V14.8148H0V0Z" fill="#ED1B24"/>
+	<path d="M35.5263 0H60V14.8148H35.5263V0Z" fill="#ED1B24"/>
+	<path d="M35.5263 25.1852H60V40H35.5263V25.1852Z" fill="#ED1B24"/>
+	</svg>
+`,
+	'sierra': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H60V40H0V0Z" fill="white"/>
+	<path d="M19.7368 13.3333H39.7368V26.6667H19.7368V13.3333Z" fill="#31358E"/>
+	</svg>
+`,
+	'tango': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H20.177V40H0V0Z" fill="#ED1B24"/>
+	<path d="M39.823 0H60V40H39.823V0Z" fill="#31358E"/>
+	<path d="M19.9115 0H40.0885V40H19.9115V0Z" fill="white"/>
+	</svg>
+`,
+	'uniform': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H30V20H0V0Z" fill="#ED1B24"/>
+	<path d="M30 0H60V20H30V0Z" fill="white"/>
+	<path d="M60 20H30V40H60V20Z" fill="#ED1B24"/>
+	<path d="M30 20H-1.90735e-06V40H30V20Z" fill="white"/>
+	</svg>
+`,
+	'victor': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H60V40H0V0Z" fill="white"/>
+	<path fill-rule="evenodd" clip-rule="evenodd" d="M7.36231 0H0V6.50378L52.858 40H60V33.3566L7.36231 0Z" fill="#ED1B24"/>
+	<path fill-rule="evenodd" clip-rule="evenodd" d="M52.6377 0H60V6.50378L7.14198 40H-3.8147e-06V33.3566L52.6377 0Z" fill="#ED1B24"/>
+	</svg>
+`,
+	'whiskey': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H60V40H0V0Z" fill="#31358E"/>
+	<path d="M12.6316 7.40741H48.1579V31.8519H12.6316V7.40741Z" fill="white"/>
+	<path d="M22.1052 14.8148H38.6842V25.1852H22.1052V14.8148Z" fill="#ED1B24"/>
+	</svg>
+`,
+	'zulu': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M60 0H0L30 20L60 0Z" fill="#FCF400"/>
+	<path d="M-3.8147e-06 40H60L30 20L-3.8147e-06 40Z" fill="#ED1B24"/>
+	<path d="M30 20L0 0V40L30 20Z" fill="black"/>
+	<path d="M30 20L60 0V40L30 20Z" fill="#31358E"/>
+	</svg>
+`,
+	'x-ray': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H60V40H0V0Z" fill="#31358E"/>
+	<path d="M0 25.1852H24.4737V40H0V25.1852Z" fill="white"/>
+	<path d="M0 0H24.4737V14.8148H0V0Z" fill="white"/>
+	<path d="M35.5263 0H60V14.8148H35.5263V0Z" fill="white"/>
+	<path d="M35.5263 25.1852H60V40H35.5263V25.1852Z" fill="white"/>
+	</svg>
+`,
 }
