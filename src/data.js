3bbe404ab304b6c13f9ab9bfb3757dd4d50bb2d9
OctOpi <64381167+OctOpiDev@users.noreply.github.com>
2024-01-19 04:07:57 +0300

    upd

--- src/data.js
+++ src/data.js
@@ -9,4 +9,23 @@ module.exports = {
 	<path d="M0 0H60L41.8421 20L60 40H0V0Z" fill="#ED1B24"/>
 	</svg>	
 `,
+	'charli': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 0H60V40H0V0Z" fill="#C4C4C4"/>
+	<path d="M0 8.14816H60V31.8519H0V8.14816Z" fill="white"/>
+	<path d="M0 31.8519H60V40H0V31.8519Z" fill="#2E3190"/>
+	<path d="M0 16.2963H60V24.4444H0V16.2963Z" fill="#ED1B24"/>
+	<path d="M0 0H60V8.14815H0V0Z" fill="#31358E"/>
+	</svg>	
+`,
+	'delta': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 12.5926H60V27.4074H0V12.5926Z" fill="#31358E"/>
+	<path d="M0 0H60V12.5926H0V0Z" fill="#FCF400"/>
+	<path d="M0 27.4074H60V40H0V27.4074Z" fill="#FCF400"/>
+	</svg>	
+`,
+	'echo': `<svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M0 20H60V40H0V20Z" fill="#ED1B24"/>
+	<path d="M0 0H60V20H0V0Z" fill="#31358E"/>
+	</svg>	
+`,
 }
